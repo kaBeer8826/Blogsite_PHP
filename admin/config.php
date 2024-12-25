6f28e9b1f72010fd2e998d017a6744061f897ea1
kabeer <studykabeer@gmail.com>
2024-12-26 01:01:25 +0530

    new project user functionality added

---
+++ admin/config.php
@@ -0,0 +1,5 @@
+<?php
+$hostname = "http://localhost/news";
+    $conn = mysqli_connect("localhost","root","","news") or die("connection Failed: " . mysqli_connect_error());
+
+?>
---
+++ admin/delete-user.php
@@ -0,0 +1,24 @@
+<?php
+include "config.php";
+
+if (!isset($_GET['id'])) {
+    die("ID Not Set");
+}
+
+$user_id = $_GET['id'];
+
+// Use prepared statements to prevent SQL injection
+$sql = "DELETE FROM user WHERE user_id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $user_id);
+
+if ($stmt->execute()) {
+    header("Location: {$hostname}/admin/users.php");
+    exit();
+} else {
+    echo "<div class='alert alert-danger'>Delete Failed</div>";
+}
+
+$stmt->close();
+$conn->close();
+?>
